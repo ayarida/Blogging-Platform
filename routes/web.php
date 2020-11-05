d3ab5b570d7fb88a2c4c52c7464f9fa8777e50a0
aya rida <aya.rida30@gmail.com>
2020-11-05 20:30:36 +0200

    Add the PostgetUpdate route

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
+Route::get('/posts',[PostController::class,'index'])->name('ListPosts');
 Route::get('post/create',[PostController::class,'create'])->name('PostGetCreate');
 Route::post('post/create', [PostController::class,'store'])->name('PostStore'); 
+Route::get('/posts/{postid}/edit',[PostController::class,'edit'])->name('PostGetUpdate'); //get the form to update the post 
+//Route::delete('/posts/{postid}',[PostController::class,'destroy'])->name('postdelete');
+//Route::patch('/posts/{postid}',[PostController::class,'update'])->name('postupdate');*/
