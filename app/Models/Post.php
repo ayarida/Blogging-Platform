24c8f4968cf188eae810adef27e1003cb01a4a3a
Aya Rida <aya.rida30@gmail.com>
2020-11-05 04:34:35 -0800

    Initial Commit

---
+++ app/Models/Post.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function comments(){
+        return $this->hasMany('App\Model\Comment');
+    }
+
+    public function likes(){
+        return $this->hasMany('App\Model\Like')->count();
+    }
+
+}
---
+++ database/migrations/2020_11_04_215020_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->String('text')->nullable(); 
+            $table->String('image')->nullable(); //url of the image
+            $table->String('type');
+            $table->unsignedBigInteger('user_id');
+            
+            $table->boolean('public')->default(true); 
+            $table->boolean('archive')->default(false); 
+            
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
